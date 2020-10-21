b58273c7bdd7b454fa533e2d7c973b0a6a4aa754
Ian <iwburns8@gmail.com>
2020-10-20 21:26:51 -0500

    add initial type definitions

--- src/log-monster-types.ts
+++ src/log-monster-types.ts
@@ -1,6 +1,30 @@
-// Import here Polyfills if needed. Recommended core-js (npm i -D core-js)
-  // import "core-js/fn/array.find"
-  // ...
-export default class DummyClass {
 
+export enum Level {
+  Trace = 1,
+  Debug = 2,
+  Info = 3,
+  Warn = 4,
+  Error = 5,
+  Fatal = 6,
+}
+
+export interface Entry {
+  level: Level,
+  group: String,
+  occurred: Date,
+  text: String,
+}
+
+export function createEntry(level: Level, group: String, occurred: Date, text: String): Entry {
+  return {
+    level,
+    group,
+    occurred,
+    text,
+  };
+}
+
+export interface LoggingStrategy {
+  log: (entry: Entry) => boolean;
+  find_logs: (level: String, group: String) => Array<Entry>;
 }
